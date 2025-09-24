c2b667b14a7bea071edd1c971d2ddb57c4a3b1db
Sariafauzani <ajas2654@gmail.com>
2025-09-24 19:06:55 +0700

    Menambahkan fitur input bilangan dari user

--- bin/cek_bilangan_prima.dart
+++ bin/cek_bilangan_prima.dart
@@ -1,4 +1,10 @@
+import 'dart:io'; // Import library untuk input/output di console
+
 void main() {
-  print("===== Program Cek Bilangan =====");
-  print("Program ini akan digunakan untuk cek bilangan prima.");
+  print("===== Program Cek Bilangan ====="); // Menampilkan judul program ke layar
+  
+  stdout.write("Masukkan sebuah bilangan: "); // Meminta user untuk memasukkan angka
+  int n = int.parse(stdin.readLineSync()!); // Membaca input user (string) lalu konversi ke integer
+
+  print("Kamu memasukkan angka: $n"); // Menampilkan kembali angka yang dimasukkan user
 }
